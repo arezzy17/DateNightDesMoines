de71736572dafa6dceb9485de46b44fe7198adc3
Adam Resnick <a.rezzy@gmail.com>
2018-10-04 11:19:37 -0500

    Add Tabs for Create Night Page

---
+++ app/src/main/java/com/example/arezz/datenightdesmoines/AddDrinksListFragment.java
@@ -0,0 +1,29 @@
+package com.example.arezz.datenightdesmoines;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddDrinksListFragment extends Fragment {
+
+
+    public AddDrinksListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_drinks_list, container, false);
+    }
+
+}
