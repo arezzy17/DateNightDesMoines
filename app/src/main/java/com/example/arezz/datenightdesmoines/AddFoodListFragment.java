550212cfe48169144122c4ac6285ab3ae0b987b8
Adam Resnick <a.rezzy@gmail.com>
2018-10-07 14:58:25 -0500

    Adding Yelp Connection for Food

--- app/src/main/java/com/example/arezz/datenightdesmoines/AddFoodListFragment.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/AddFoodListFragment.java
@@ -9,18 +9,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddFoodListFragment extends Fragment {
+public class AddFoodListFragment extends Fragment implements IYelpList{
 
     private RecyclerView foodList;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.Adapter yelpItemAdapter;
-
+    private ArrayList<YelpItem> foodItems;
     public AddFoodListFragment() {
         // Required empty public constructor
     }
@@ -32,20 +43,45 @@ public class AddFoodListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_food_list, container, false);
 
-        ArrayList<YelpItem> foodItems = new ArrayList<YelpItem>();
+        foodItems = new ArrayList<YelpItem>();
         foodList = (RecyclerView) view.findViewById(R.id.food_list);
 
-        foodItems.add((new YelpItem("Test","4")));
-        foodItems.add((new YelpItem("Test2","3")));
-
-
+        // Get a RequestQueue
+        RequestQueue queue = MySingleton.getInstance(getContext().getApplicationContext()).getRequestQueue();
+        JsonObjectRequest myReq = MySingleton.getInstance(getContext()).GetJsonRequestFromUrl("https://api.yelp.com/v3/businesses/search","Bearer baYflpcDgbIhpcxDzfCTVY-8-MNrTaQKs-Xi7TkguApK9CW1ezFdxhlNAS754U7dQEou-gJzbZkP54dNIrFO_70lrO1cIcNS0ziaZBqslfvysRtzBZ04M-LFYt23W3Yx","Des Moines, IA", "italian", this );
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        MySingleton.getInstance(getContext()).addToRequestQueue(myReq);
+        String req = myReq.toString();
         layoutManager = new LinearLayoutManager(getContext());
         foodList.setLayoutManager(layoutManager);
+
 
         yelpItemAdapter = new YelpItemAdapter(getContext(), foodItems);
         foodList.setAdapter(yelpItemAdapter);
 
         return view;
+    }
+
+    public void setList(JSONObject jsonObject){
+        JSONArray businesses = new JSONArray();
+        try{
+            businesses=jsonObject.getJSONArray("businesses");
+        }
+        catch (Exception ex){
+
+        }
+        foodItems = new ArrayList<YelpItem>();
+        for(int i = 0; i<businesses.length();i++){
+            try{
+                foodItems.add(new YelpItem(businesses.getJSONObject(i).getString("name"),businesses.getJSONObject(i).getDouble("rating")+""));
+            }
+            catch (Exception ex){
+
+            }
+        }
+
+        yelpItemAdapter = new YelpItemAdapter(getContext(), foodItems);
+        foodList.setAdapter(yelpItemAdapter);
     }
 
 }
---
+++ app/src/main/java/com/example/arezz/datenightdesmoines/IYelpList.java
@@ -0,0 +1,9 @@
+package com.example.arezz.datenightdesmoines;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public interface IYelpList {
+
+    public void setList(JSONObject jsonObject);
+}
