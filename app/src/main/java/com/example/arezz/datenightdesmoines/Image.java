0a421215cbc870070d1ebabf86d7535c15171f76
gkercher <33293645+gkercher@users.noreply.github.com>
2018-10-12 08:52:39 -0500

    Added class from Realm Strings since RealmList does not support Strings as well as a class for a Night and a Class for Images

---
+++ app/src/main/java/com/example/arezz/datenightdesmoines/Image.java
@@ -0,0 +1,16 @@
+package com.example.arezz.datenightdesmoines;
+
+import android.graphics.Bitmap;
+
+public class Image {
+    private String id;
+    private Bitmap imageBitmap;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
