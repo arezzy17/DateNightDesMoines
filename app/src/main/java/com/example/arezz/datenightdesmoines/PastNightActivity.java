21578cd360064a4e657500f81abc9bad9dbc6c78
gkercher <33293645+gkercher@users.noreply.github.com>
2018-10-21 14:28:35 -0500

    Created Past Night Activity

---
+++ app/src/main/java/com/example/arezz/datenightdesmoines/PastNightActivity.java
@@ -0,0 +1,13 @@
+package com.example.arezz.datenightdesmoines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PastNightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_night);
+    }
+}
