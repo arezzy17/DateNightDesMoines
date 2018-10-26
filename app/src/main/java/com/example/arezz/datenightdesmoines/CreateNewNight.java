993d6d98f062f32ba44e49030f4e0b0c0c7bb67e
gkercher <33293645+gkercher@users.noreply.github.com>
2018-10-26 00:11:35 -0500

    Realm functionality and login functionality

--- app/src/main/java/com/example/arezz/datenightdesmoines/CreateNewNight.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/CreateNewNight.java
@@ -1,5 +1,6 @@
 package com.example.arezz.datenightdesmoines;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
