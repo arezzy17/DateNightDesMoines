5b5bbf68c63303c7eceab758a86813b243331c35
JasonWallerDrake <42783039+JasonWallerDrake@users.noreply.github.com>
2018-10-26 11:50:08 -0500

    adapter
    adapter partially set-up

--- app/src/main/java/com/example/arezz/datenightdesmoines/ConfirmNightActivity.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/ConfirmNightActivity.java
@@ -3,11 +3,15 @@ package com.example.arezz.datenightdesmoines;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ConfirmNightActivity extends AppCompatActivity {
 
@@ -18,6 +22,8 @@ public class ConfirmNightActivity extends AppCompatActivity {
     private RecyclerView NightList;
     private Button HomeButton;
     private EditText DateSelector;
+    private RecyclerView.Adapter confirmAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
 
 
@@ -54,9 +60,27 @@ public class ConfirmNightActivity extends AppCompatActivity {
         ConfirmNightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), CurrentNightActivity.class);
+                Intent intent = new Intent(getBaseContext(), CurrentNight.class);
                 startActivity(intent);
             }
         });
+
+        final ArrayList<Event> events = new ArrayList<Event>();
+
+        RecyclerViewClickListener listener = new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Event event = (Event) events.get(position);
+                Intent intent = new Intent(view.getContext(), LoginActivity.class); // navigate to details card once it's created
+                intent.putExtra("event",(Serializable)event);
+                startActivity(intent);
+            }
+        };
+
+        layoutManager = new LinearLayoutManager(this);
+        NightList.setLayoutManager(layoutManager);
+
+        confirmAdapter = new ConfirmNightAdapter(this, events, listener);
+        NightList.setAdapter(confirmAdapter);
     }
-}
+}w
--- app/src/main/java/com/example/arezz/datenightdesmoines/ConfirmNightAdapter.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/ConfirmNightAdapter.java
@@ -4,38 +4,62 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-public class ConfirmNightAdapter extends RecyclerView.Adapter<ConfirmNightAdapter>NightViewHolder{
+public class ConfirmNightAdapter extends RecyclerView.Adapter<ConfirmNightAdapter.ConfirmNightViewHolder> {
     private Context context;
-    private ArrayList<Night> nights;
+    private ArrayList<Event> events;
+    private RecyclerViewClickListener mListener;
 
-    public ConfirmNightAdapter(Context context, ArrayList<Night> dataSet) {
+
+    public ConfirmNightAdapter(Context context, ArrayList<Event> dataSet) {
         this.context = context;
-        this.nights = dataSet;
+        this.events = dataSet;
+        this.mListener = clickListener;
     }
 
-    public static class NightViewHolder extends RecyclerView.ViewHolder {
-        //public
+    public static class ConfirmNightViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView eventNameView;
+        public TextView startTimeView;
+        public TextView endTimeView;
+        private RecyclerViewClickListener mListener;
+
+        public ConfirmNightViewHolder(View v, RecyclerViewClickListener listener) {
+            super(v);
+            eventNameView = v.findViewById(R.id.event_name_confirm);
+            endTimeView = v.findViewById(R.id.event_end_confirm);
+            startTimeView = v.findViewById(R.id.event_start_confirm);
+            mListener = listener;
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            mListener.onClick(view, getAdapterPosition());
+        }
     }
 
 
-@Override
-public int getItemCount() {
-    return nights.size();
-}
+        @Override
+        public int getItemCount() {
+            return events.size();
+        }
 
-@Override
-public ConfirmNightAdapter.NightViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    View v = (View) LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.night_cell, parent, false);
-    NightViewHolder vh = new NightViewHolder(v);
-    return vh;
-}
+        @Override
+        public ConfirmNightAdapter.ConfirmNightViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = (View) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.confirm_cell, parent, false);
+            ConfirmNightViewHolder vh = new ConfirmNightViewHolder(v);
+            return vh;
+        }
 
-@Override
-public void onBindViewHolder(NightViewHolder holder, int position) {
-   // holder.????.setText(nights.get(position).getName());
-    //holder.ageView.setText(bulldogs.get(postion).getAge());
-}
-}
+        @Override
+        public void onBindViewHolder(ConfirmNightViewHolder holder, int position) {
+            // holder.????.setText(nights.get(position).getName());
+            //holder.ageView.setText(bulldogs.get(postion).getAge());
+            holder.eventNameView.setText(events.get(position).getEventName());
+            holder.startTimeView.setText((CharSequence) events.get(position).getStartTime());
+            holder.endTimeView.setText((CharSequence) events.get(position).getEndTime());
+        }
+    }
