85ed498c7a997f99225537925d8a2aeafa27856f
JasonWallerDrake <42783039+JasonWallerDrake@users.noreply.github.com>
2018-10-28 15:55:08 -0500

    Review Rating Bar
    Set up the rating bar functionality in Review

--- app/src/main/java/com/example/arezz/datenightdesmoines/ReviewActivity.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/ReviewActivity.java
@@ -1,6 +1,9 @@
 package com.example.arezz.datenightdesmoines;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +15,11 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
+import io.realm.Realm;
 
 public class ReviewActivity extends AppCompatActivity {
 
@@ -31,13 +39,20 @@ public class ReviewActivity extends AppCompatActivity {
         nightName = (TextView) findViewById(R.id.night_name_review);
         nightImage = (ImageView) findViewById(R.id.night_image);
         ratingBar = (RatingBar) findViewById(R.id.ratingBar_review);
-        commentReview = (EditText) findViewById(R.id.comment_review);
         homeButton = (ImageButton) findViewById(R.id.home_button);
+
+        Realm realm = Realm.getDefaultInstance();
+        final String nightId = getIntent().getStringExtra("nightId");
+        Night night = realm.where(Night.class).equalTo("Id", nightId).findFirst();
+
+        nightName.setText(night.getDateName());
+
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), TopRatedActivity.class);
+                saveRating(nightId);
                 startActivity(intent);
             }
         });
@@ -46,7 +61,29 @@ public class ReviewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), TopRatedActivity.class);
+                saveRating(nightId);
                 startActivity(intent);
+
+            }
+        });
+
+
+        if(night.getImageId() != null) {
+            Image image = realm.where(Image.class).equalTo("id", night.getImageId()).findFirst();
+            Bitmap bmp = BitmapFactory.decodeByteArray(image.getImageBitmap(),0,image.getImageBitmap().length);
+            nightImage.setImageBitmap(bmp);
+        }
+    }
+
+    public void saveRating(final String nightId) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Night night = realm.where(Night.class).equalTo("Id", nightId).findFirst();
+                Float ratingNumber = ratingBar.getRating();
+                night.setRating(ratingNumber);
+                realm.copyToRealm(night);
             }
         });
     }
