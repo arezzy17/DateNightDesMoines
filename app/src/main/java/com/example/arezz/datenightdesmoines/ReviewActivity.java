cdc5c3dcca8f9b791fa45d5ee4f79809586cdbc5
Adam Resnick <a.rezzy@gmail.com>
2018-10-28 12:40:17 -0500

    Change review night button activity

--- app/src/main/java/com/example/arezz/datenightdesmoines/ReviewActivity.java
+++ app/src/main/java/com/example/arezz/datenightdesmoines/ReviewActivity.java
@@ -45,7 +45,7 @@ public class ReviewActivity extends AppCompatActivity {
         completeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), CreateNewNight.class);
+                Intent intent = new Intent(getBaseContext(), TopRatedActivity.class);
                 startActivity(intent);
             }
         });
